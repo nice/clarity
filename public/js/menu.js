4f2ba142a4c4d4941f3c5c0cf0ac62087808742b
Jayaram <jayaram@gmx.com>
2024-03-03 01:20:51 +0530

    adds responsive menu

---
+++ public/js/menu.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function () {
+	var burger = document.querySelector("#burger");
+	var menu = document.querySelector("#menu");
+
+	if (burger && menu) {
+		burger.addEventListener("click", function () {
+			console.log("fff");
+			menu.classList.toggle("hidden");
+		});
+	}
+});
