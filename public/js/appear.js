f405fd9c25b6c9a8e03eb8e89fdf76a2b985e197
Jayaram <jayaram@gmx.com>
2024-03-03 09:25:44 +0530

    adds seo component, upgrades image appear logic

--- public/js/appear.js
+++ public/js/appear.js
@@ -1,8 +1,22 @@
+/*
+  Image fade in animation 
+  Set img { opacity: 0} is set to get consistent results for loaded and cached images.
+
+  Add a noscript to
+  <noscript><style>img { opacity: 1 !important; }</style></noscript>
+
+*/
+
 const images = document.getElementsByTagName("img");
 
 for (let image of images) {
   image.addEventListener("load", appear);
   image.style.opacity = "0";
+
+  // for cached images
+  if (image.complete) {
+    appear.bind(image)();
+  }
 }
 
 function appear() {
