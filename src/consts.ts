f007869d12a60ad517bf265024ed72233890d269
Jayaram <jayaram@gmx.com>
2024-03-05 21:32:49 +0530

    updates default site image

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,7 @@
 
 export const SITE_TITLE = "Clarity";
 export const SITE_DESCRIPTION = "Minimal blog theme for Astro!";
-export const SITE_IMAGE = "/placeholder-site-image.png";
+export const SITE_IMAGE = "/placeholder-site-image.jpg";
 
 export const ACCENT_BACKGROUND_COLOR = "#57534e";
 export const ACCENT_TEXT_COLOR = "#ffffff";
