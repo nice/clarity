5fd20576f1bc4f68241136a892b4d9d03600ed2f
Jayaram <jayaram@gmx.com>
2024-03-06 16:13:04 +0530

    fixes menu init

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,15 +1,13 @@
 function initMenu() {
-  document.addEventListener("DOMContentLoaded", function () {
-    var burger = document.querySelector("#burger");
-    var menu = document.querySelector("#menu");
+  var burger = document.querySelector("#burger");
+  var menu = document.querySelector("#menu");
 
-    if (burger && menu) {
-      burger.addEventListener("click", function () {
-        console.log("fff");
-        menu.classList.toggle("hidden");
-      });
-    }
-  });
+  if (burger && menu) {
+    burger.addEventListener("click", function () {
+      console.log("fff");
+      menu.classList.toggle("hidden");
+    });
+  }
 }
 
-initMenu();
+document.addEventListener("DOMContentLoaded", initMenu);
