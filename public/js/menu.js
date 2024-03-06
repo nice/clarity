467eb7076922abced716b8e5a4dbe7eefb219f21
Jayaram <jayaram@gmx.com>
2024-03-06 16:01:38 +0530

    updates menu js

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,11 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-  var burger = document.querySelector("#burger");
-  var menu = document.querySelector("#menu");
+function initMenu() {
+  document.addEventListener("DOMContentLoaded", function () {
+    var burger = document.querySelector("#burger");
+    var menu = document.querySelector("#menu");
 
-  if (burger && menu) {
-    burger.addEventListener("click", function () {
-      console.log("fff");
-      menu.classList.toggle("hidden");
-    });
-  }
-});
+    if (burger && menu) {
+      burger.addEventListener("click", function () {
+        console.log("fff");
+        menu.classList.toggle("hidden");
+      });
+    }
+  });
+}
+
+initMenu();
